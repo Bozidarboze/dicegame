1a4b831a1870c2ccda38bd20102953885f55f197
Bozidarboze <Bozidar_boze123@hotmail.com>
2020-04-02 10:04:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,47 @@
 var dice1 = 0, dice2 = 0, dice3 = 0, dice4 = 0, dice5 = 0, dice6 = 0, dice7 = 0, dice8 = 0,
-    dices1, dices2, score1 = 0, score2 = 0, score3 = 0, score4 = 0, winner;
+    dices1, dices2, score1 = 0, score2 = 0, score3 = 0, score4 = 0, winner, draw;
 
 $(".p1").click(function () {
-    playerRoll(".dice1", ".dice2", ".p1");
+    playerRoll(".dice1", ".dice2", ".p1", ".p2", ".p3", ".p4", dice3, dice5, dice7);
     dice1 = dices1;
     dice2 = dices2;
     score1 = dice1 + dice2;
     checkWinner();
     setWinner();
-    $(".p2").prop("disabled", true);
-    $(".p3").prop("disabled", true);
-    $(".p4").prop("disabled", true);
-    setTimeout(() => {
-        if (dice3 == 0) { $(".p2").prop("disabled", false); }
-        if (dice5 == 0) { $(".p3").prop("disabled", false); }
-        if (dice7 == 0) { $(".p4").prop("disabled", false); }
-    }, 600);
 });
 
 $(".p2").click(function () {
-    playerRoll(".dice3", ".dice4", ".p2");
+    playerRoll(".dice3", ".dice4", ".p2", ".p1", ".p3", ".p4", dice1, dice5, dice7);
     dice3 = dices1;
     dice4 = dices2;
     score2 = dice3 + dice4;
     checkWinner();
     setWinner();
-    $(".p1").prop("disabled", true);
-    $(".p3").prop("disabled", true);
-    $(".p4").prop("disabled", true);
-    setTimeout(() => {
-        if (dice1 == 0) { $(".p1").prop("disabled", false); }
-        if (dice5 == 0) { $(".p3").prop("disabled", false); }
-        if (dice7 == 0) { $(".p4").prop("disabled", false); }
-    }, 600);
 });
 
 $(".p3").click(function () {
-    playerRoll(".dice5", ".dice6", ".p3");
+    playerRoll(".dice5", ".dice6", ".p3", ".p1", ".p2", ".p4", dice1, dice3, dice7);
     dice5 = dices1;
     dice6 = dices2;
     score3 = dice5 + dice6;
     checkWinner();
     setWinner();
-    $(".p1").prop("disabled", true);
-    $(".p2").prop("disabled", true);
-    $(".p4").prop("disabled", true);
-    setTimeout(() => {
-        if (dice1 == 0) { $(".p1").prop("disabled", false); }
-        if (dice3 == 0) { $(".p2").prop("disabled", false); }
-        if (dice7 == 0) { $(".p4").prop("disabled", false); }
-    }, 600);
 });
 
 $(".p4").click(function () {
-    playerRoll(".dice7", ".dice8", ".p4");
+    playerRoll(".dice7", ".dice8", ".p4", ".p1", ".p2", ".p3", dice1, dice3, dice5);
     dice7 = dices1;
     dice8 = dices2;
     score4 = dice7 + dice8;
     checkWinner();
     setWinner();
-    $(".p1").prop("disabled", true);
-    $(".p2").prop("disabled", true);
-    $(".p3").prop("disabled", true);
-    setTimeout(() => {
-        if (dice1 == 0) { $(".p1").prop("disabled", false); }
-        if (dice3 == 0) { $(".p2").prop("disabled", false); }
-        if (dice5 == 0) { $(".p3").prop("disabled", false); }
-    }, 600);
 });
 
 $("#reset").click(function () {
     reset();
 });
 
-function playerRoll(dicec1, dicec2, buttonPressed) {
+function playerRoll(dicec1, dicec2, buttonPressed, button1, button2, button3, bd1, bd2, bd3) {
     $(buttonPressed).prop("disabled", true);
     dices1 = Math.floor(Math.random() * 6) + 1;
     dices2 = Math.floor(Math.random() * 6) + 1;
@@ -112,6 +80,17 @@ function playerRoll(dicec1, dicec2, buttonPressed) {
         $(dicec2).attr("src", "images/dice" + dices2 + ".png");
     }, 400);
 
+    if (draw != true) {
+        $(button1).prop("disabled", true);
+        $(button2).prop("disabled", true);
+        $(button3).prop("disabled", true);
+        setTimeout(() => {
+            if (bd1 == 0 && draw != true) { $(button1).prop("disabled", false); }
+            if (bd2 == 0 && draw != true) { $(button2).prop("disabled", false); }
+            if (bd3 == 0 && draw != true) { $(button3).prop("disabled", false); }
+        }, 600);
+    }
+
     var diceRollSound = new Audio("sounds/dice-roll.mp3");
     diceRollSound.play();
 };
@@ -119,14 +98,43 @@ function playerRoll(dicec1, dicec2, buttonPressed) {
 function checkWinner() {
     if (score1 > score2 && score1 > score3 && score1 > score4) {
         winner = "🏆Winner is Player 1!🏆";
+        draw = false;
     } else if (score2 > score1 && score2 > score3 && score2 > score4) {
         winner = "🏆Winner is Player 2!🏆";
+        draw = false;
     } else if (score3 > score1 && score3 > score2 && score3 > score4) {
         winner = "🏆Winner is Player 3!🏆";
+        draw = false;
     } else if (score4 > score1 && score4 > score2 && score4 > score3) {
         winner = "🏆Winner is Player 4!🏆";
+        draw = false;
     } else {
         winner = "Draw!";
+        if (score1 == score2) {
+            $(".p1").prop("disabled", false);
+            $(".p2").prop("disabled", false);
+            draw = true;
+        } else if (score1 == score3) {
+            $(".p1").prop("disabled", false);
+            $(".p3").prop("disabled", false);
+            draw = true;
+        } else if (score1 == score4) {
+            $(".p1").prop("disabled", false);
+            $(".p4").prop("disabled", false);
+            draw = true;
+        } else if (score2 == score3) {
+            $(".p2").prop("disabled", false);
+            $(".p3").prop("disabled", false);
+            draw = true;
+        } else if (score2 == score4) {
+            $(".p2").prop("disabled", false);
+            $(".p4").prop("disabled", false);
+            draw = true;
+        } else if (score3 == score4) {
+            $(".p3").prop("disabled", false);
+            $(".p4").prop("disabled", false);
+            draw = true;
+        }
     }
 };
 
@@ -142,7 +150,7 @@ function setWinner() {
 
 function reset() {
     dice1 = 0, dice2 = 0, dice3 = 0, dice4 = 0, dice5 = 0, dice6 = 0, dice7 = 0, dice8 = 0,
-        score1 = 0, score2 = 0, score3 = 0, score4 = 0;
+        score1 = 0, score2 = 0, score3 = 0, score4 = 0, draw = false;
 
     $(".dice").attr("src", "images/dice.png");
     $("h1").text("Dice Game");
@@ -150,6 +158,11 @@ function reset() {
     $(".p2").prop("disabled", false);
     $(".p3").prop("disabled", false);
     $(".p4").prop("disabled", false);
+}
+
+function saveScore() {
+    save1 = score1;
+    save2 = score2;
 }
 
 $("#players2").click(function () {
@@ -167,7 +180,6 @@ $("#players3").click(function () {
     $(".players4").addClass("hidden");
     $("#players2").removeClass("active");
     $("#players4").removeClass("active");
-    $("img").addClass("width3");
     reset();
 });
 
