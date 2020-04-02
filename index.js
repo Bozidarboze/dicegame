908a9175f68ad9b6da9185f3492007372a576814
Bozidarboze <Bozidar_boze123@hotmail.com>
2020-04-02 08:29:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,78 +2,50 @@ var dice1 = 0, dice2 = 0, dice3 = 0, dice4 = 0, dice5 = 0, dice6 = 0, dice7 = 0,
     dices1, dices2, score1 = 0, score2 = 0, score3 = 0, score4 = 0, winner;
 
 $(".p1").click(function () {
-    playerRoll(".dice1", ".dice2", ".p1");
+    playerRoll(".dice1", ".dice2", ".p1", ".p2", ".p3", ".p4", dice3, dice5, dice7);
     dice1 = dices1;
     dice2 = dices2;
     score1 = dice1 + dice2;
     checkWinner();
     setWinner();
-    $(".p2").prop("disabled", true);
-    $(".p3").prop("disabled", true);
-    $(".p4").prop("disabled", true);
-    setTimeout(() => {
-        if (dice3 == 0) { $(".p2").prop("disabled", false); }
-        if (dice5 == 0) { $(".p3").prop("disabled", false); }
-        if (dice7 == 0) { $(".p4").prop("disabled", false); }
-    }, 600);
+
 });
 
 $(".p2").click(function () {
-    playerRoll(".dice3", ".dice4", ".p2");
+    playerRoll(".dice3", ".dice4", ".p2", ".p1", ".p3", ".p4", dice1, dice5, dice7);
     dice3 = dices1;
     dice4 = dices2;
     score2 = dice3 + dice4;
     checkWinner();
     setWinner();
-    $(".p1").prop("disabled", true);
-    $(".p3").prop("disabled", true);
-    $(".p4").prop("disabled", true);
-    setTimeout(() => {
-        if (dice1 == 0) { $(".p1").prop("disabled", false); }
-        if (dice5 == 0) { $(".p3").prop("disabled", false); }
-        if (dice7 == 0) { $(".p4").prop("disabled", false); }
-    }, 600);
+
 });
 
 $(".p3").click(function () {
-    playerRoll(".dice5", ".dice6", ".p3");
+    playerRoll(".dice5", ".dice6", ".p3", ".p1", ".p2", ".p4", dice1, dice3, dice7);
     dice5 = dices1;
     dice6 = dices2;
     score3 = dice5 + dice6;
     checkWinner();
     setWinner();
-    $(".p1").prop("disabled", true);
-    $(".p2").prop("disabled", true);
-    $(".p4").prop("disabled", true);
-    setTimeout(() => {
-        if (dice1 == 0) { $(".p1").prop("disabled", false); }
-        if (dice3 == 0) { $(".p2").prop("disabled", false); }
-        if (dice7 == 0) { $(".p4").prop("disabled", false); }
-    }, 600);
+
 });
 
 $(".p4").click(function () {
-    playerRoll(".dice7", ".dice8", ".p4");
+    playerRoll(".dice7", ".dice8", ".p4", ".p1", ".p2", ".p3", dice1, dice3, dice5);
     dice7 = dices1;
     dice8 = dices2;
     score4 = dice7 + dice8;
     checkWinner();
     setWinner();
-    $(".p1").prop("disabled", true);
-    $(".p2").prop("disabled", true);
-    $(".p3").prop("disabled", true);
-    setTimeout(() => {
-        if (dice1 == 0) { $(".p1").prop("disabled", false); }
-        if (dice3 == 0) { $(".p2").prop("disabled", false); }
-        if (dice5 == 0) { $(".p3").prop("disabled", false); }
-    }, 600);
+
 });
 
 $("#reset").click(function () {
     reset();
 });
 
-function playerRoll(dicec1, dicec2, buttonPressed) {
+function playerRoll(dicec1, dicec2, buttonPressed, button1, button2, button3, bd1, bd2, bd3) {
     $(buttonPressed).prop("disabled", true);
     dices1 = Math.floor(Math.random() * 6) + 1;
     dices2 = Math.floor(Math.random() * 6) + 1;
@@ -112,6 +84,17 @@ function playerRoll(dicec1, dicec2, buttonPressed) {
         $(dicec2).attr("src", "images/dice" + dices2 + ".png");
     }, 400);
 
+
+    $(button1).prop("disabled", true);
+    $(button2).prop("disabled", true);
+    $(button3).prop("disabled", true);
+    setTimeout(() => {
+        if (bd1 == 0) { $(button1).prop("disabled", false); }
+        if (bd2 == 0) { $(button2).prop("disabled", false); }
+        if (bd3 == 0) { $(button3).prop("disabled", false); }
+    }, 600);
+
+
     var diceRollSound = new Audio("sounds/dice-roll.mp3");
     diceRollSound.play();
 };
@@ -127,6 +110,7 @@ function checkWinner() {
         winner = "🏆Winner is Player 4!🏆";
     } else {
         winner = "Draw!";
+
     }
 };
 
@@ -167,7 +151,6 @@ $("#players3").click(function () {
     $(".players4").addClass("hidden");
     $("#players2").removeClass("active");
     $("#players4").removeClass("active");
-    $("img").addClass("width3");
     reset();
 });
 
